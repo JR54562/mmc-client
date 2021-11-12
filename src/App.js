fc9693dae782fcbbf6e0a50b54171bced8cb2c37
Jay Reische <ReischeJay@JohnDeere.com>
2021-11-12 12:23:47 -0600

    main rendering in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import React, { Component } from "react";
+import { Route } from 'react-router-dom';
 import "bootswatch/dist/yeti/bootstrap.min.css";
 import Header from './components/Header';
 import "./App.css";
+import Main from "./components/Main";
 
 export default class App extends Component {
   constructor() {
@@ -16,10 +18,33 @@ export default class App extends Component {
     };
   }
 
+handleChange = (e) => {
+    const value = e.target.value;
+    e.preventDefault();
+    this.setState({
+      ...this.state,
+      [e.target.name]: value,
+    });
+    console.log("username", this.state.username);
+    console.log("password", this.state.password);
+    console.log("confirm", this.state.confirmedpassword);
+  };
+
+
+
   render() {
     return (
       <div className="App">
         <Header isLoggedIn={this.state.isLoggedIn} logout={this.logout} />
+        <Route
+          path="/"
+          render={(routerProps) => (
+            <Main />
+          )
+}
+        />
+
+
         </div>
         )
         }
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Main(props) {
+    return (
+        <div>
+           <img
+						className="collection"
+						src="/images/collection.png"
+						alt="collection of movies"
+					/> 
+        </div>
+    );
+}
+
